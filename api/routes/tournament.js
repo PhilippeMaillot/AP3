8d0c553eafca1516190cce324ef68db781de6ab8
Philippe <maillotphilippe78@gmail.com>
2023-12-22 17:35:39 +0100

    Voir les tournois

--- api/routes/tournament.js
+++ api/routes/tournament.js
@@ -18,18 +18,35 @@ router.get("/", (req, res) => {
 });
 
 router.get("/participations", (req, res) => {
-  db.query("SELECT * FROM tournamentparticipation", (err, results) => {
-    if (err) {
-      console.error(
-        "Erreur lors de la récupération des données : " + err.message
-      );
-      res.status(500).json({ err: "Erreur serveur" });
-      return;
-    }
+  // Si le paramètre id_tournament est présent dans la requête
+  if (req.query.id_tournament) {
+    const tournamentId = req.query.id_tournament;
 
-    res.status(200).json(results);
-  });
+    // Utilisez tournamentId dans votre requête SQL pour filtrer les résultats
+    db.query(
+      "SELECT tournamentparticipation.*, clubs.* " +
+      "FROM tournamentparticipation " +
+      "INNER JOIN clubs ON tournamentparticipation.id_club = clubs.id_club " +
+      "WHERE tournamentparticipation.id_tournament = ?",
+      [tournamentId],
+      (err, results) => {
+        if (err) {
+          console.error(
+            "Erreur lors de la récupération des données : " + err.message
+          );
+          res.status(500).json({ err: "Erreur serveur" });
+          return;
+        }
+
+        res.status(200).json(results);
+      }
+    );
+  } else {
+    // Si le paramètre id_tournament n'est pas présent, renvoyez une erreur 400 Bad Request
+    res.status(400).json({ err: "Paramètre id_tournament manquant" });
+  }
 });
+
 
 router.post("/add", async (req, res) => {
   try {
