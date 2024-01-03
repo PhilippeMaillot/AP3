2f5b3c00b044a020808d197650f98039f6e06737
Tristandac28 <tdacier@siclade.fr>
2024-01-03 10:56:21 +0100

    fix calendar

--- api/models/tournamentModel.js
+++ api/models/tournamentModel.js
@@ -6,6 +6,22 @@ class TournamentModel {
       "INSERT INTO tournament (tournament_name, tournament_date, id_field) VALUES ( ?, ?, ?)";
     db.query(query, [tournament_name, tournament_date, tournament_field], cb);
   }
+  getClubs = async () => {
+    return new Promise((resolve, reject) => {
+      db.query("SELECT * FROM tournament", (err, results) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(results);
+      });
+    });
+  };
+  static addClubTournament(db, req, cb){
+    const { id_tournament, id_club } = req.body;
+    const query = "INSERT INTO tournamentparticipation (id_tournament, id_club) VALUES (?, ?)";
+    db.query(query, [id_tournament, id_club], cb);
+  }
 }
 
 module.exports = TournamentModel;
